ffa266154769a0c711afade0be3eddaa734ac99a
loyalfish <70802120+loyalfish@users.noreply.github.com>
2020-10-13 10:48:12 +0700

    Add files via upload

---
+++ 13.10.2020№2.py
@@ -0,0 +1,9 @@
+print("Ведите натуральное число:")
+a = int(input())
+if a % 400 == 0 and a % 100 != 0:
+    print("YES")
+elif a % 4 == 0 and a % 100 != 0:
+    print("YES")
+else:
+    print("NO")
+
